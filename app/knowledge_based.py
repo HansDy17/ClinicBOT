fbbcdfa368af0cd7ca80f650c024520f80b6c53d
Hans Benson Dy <hansbenson.dytuan@g.msuiit.edu.ph>
2025-01-27 08:18:53 +0800

    BUILD: Initial knowledge-based file

---
+++ app/knowledge_based.py
@@ -0,0 +1,45 @@
+from phi.knowledge.combined import CombinedKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+
+url_pdf_knowledge_base = PDFUrlKnowledgeBase(
+    urls=["pdf_url"],
+    # Table name: ai.pdf_documents
+    vector_db=PgVector(
+        table_name="pdf_documents",
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+    ),
+)
+
+website_knowledge_base = WebsiteKnowledgeBase(
+    urls=["https://www.msuiit.edu.ph/offices/clinic/index.php"],
+    # Number of links to follow from the seed URLs
+    max_links=10,
+    # Table name: ai.website_documents
+    vector_db=PgVector(
+        table_name="website_documents",
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+    ),
+)
+
+local_pdf_knowledge_base = PDFKnowledgeBase(
+    path="data/pdfs",
+    # Table name: ai.pdf_documents
+    vector_db=PgVector(
+        table_name="pdf_documents",
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+    ),
+    reader=PDFReader(chunk=True),
+)
+
+knowledge_base = CombinedKnowledgeBase(
+    sources=[
+        url_pdf_knowledge_base,
+        website_knowledge_base,
+        local_pdf_knowledge_base,
+    ],
+    vector_db=PgVector(
+        # Table name: ai.combined_documents
+        table_name="combined_documents",
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+    ),
+)
