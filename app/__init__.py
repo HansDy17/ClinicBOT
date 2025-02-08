cc6af14eb73eaba87ce8dd954441a8335dc06a98
Hans Benson Dy <hansbenson.dytuan@g.msuiit.edu.ph>
2025-02-08 15:43:16 +0800

    FEAT: Implement application factory pattern and initialize Flask app with configuration

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+#from flask_mysql_connector import MySQL
+# from flask_bootstrap import Bootstrap
+from app.config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
+
+# mysql = MySQL()
+# bootstrap = Bootstrap()
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=SECRET_KEY,
+        MYSQL_USER=DB_USERNAME,
+        MYSQL_PASSWORD=DB_PASSWORD,
+        MYSQL_DATABASE=DB_NAME,
+        MYSQL_HOST=DB_HOST, 
+        BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
+    )
+          
+    from .routes.index import index_bp
+
+    app.register_blueprint(index_bp, url_prefix='/')
+
+    return app
---
+++ app/agents/knowledge_base.py
@@ -0,0 +1,48 @@
+from phi.knowledge.combined import CombinedKnowledgeBase
+from phi.knowledge.website import WebsiteKnowledgeBase
+from phi.knowledge.pdf import PDFKnowledgeBase, PDFReader
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+from agno.embedder.ollama import OllamaEmbedder
+
+db_url="postgresql+psycopg://postgres:1234@localhost:5432/knowledge_base"
+
+# url_pdf_knowledge_base = PDFUrlKnowledgeBase(
+#     urls=["pdf_url"],
+#     # Table name: ai.pdf_documents
+#     vector_db=PgVector(
+#         table_name="pdf_documents",
+#         db_url="postgresql+psycopg://root:1234@localhost:5432/knowledge_base",
+#     ),
+# )
+
+website_knowledge_base = WebsiteKnowledgeBase(
+    urls=["https://www.msuiit.edu.ph/offices/clinic/index.php"],
+    # Number of links to follow from the seed URLs
+    max_links=20,
+    # Table name: ai.website_documents
+    vector_db=PgVector(
+        table_name="website_documents",
+        db_url=db_url,
+        embedder=OllamaEmbedder(id="nomic-embed-text")
+    ),
+)
+
+local_pdf_knowledge_base = PDFKnowledgeBase(
+    path="Selfcare-PDFs",
+    # Table name: ai.pdf_documents
+
+)
+
+knowledge_base = CombinedKnowledgeBase(
+    sources=[
+        # url_pdf_knowledge_base,
+        website_knowledge_base,
+        local_pdf_knowledge_base,
+    ],
+    vector_db=PgVector(
+        # Table name: ai.combined_documents
+        table_name="combined_documents",
+        db_url=db_url,
+    ),
+)
