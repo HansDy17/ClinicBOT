c789427ab78ea223c89cc36bda1dae4fa772c968
Hans Benson Dy <hansbenson.dytuan@g.msuiit.edu.ph>
2025-04-08 11:07:57 +0800

    FEAT: Initialize Flask-Mail in the application setup

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ mail = Mail()
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
+    mail.init_app(app)
     app.config.from_mapping(
         SECRET_KEY=SECRET_KEY,
         MYSQL_USER=DB_USERNAME,
